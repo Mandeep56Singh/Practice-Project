08b678f792726b61c2e0fc49c09019f984a5bb87
mandeepSingh <mandeepraj2312@gmail.com>
2025-03-18 17:46:33 +0530

    refactor: remove unneccessary files

--- apps/Todo-Backend-Mongo/src/dtos/todo.types.ts
+++
@@ -1,7 +0,0 @@
-export type TodoResponse = {
-  id: string;
-  text: string;
-  completed: boolean;
-  priority: "LOW" | "MEDIUM" | "HIGH";
-  date: Date;
-};
--- apps/Todo-Backend-Mongo/src/routes/health.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { HealthController } from "../controller/health.controller.js";
-
-const router = Router();
-const healthController = new HealthController();
-router.get("/server", healthController.healthCheckServer);
-router.get("/db", healthController.healthCheckDB);
-
-export default router;
